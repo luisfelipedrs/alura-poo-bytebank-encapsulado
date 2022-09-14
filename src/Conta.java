0578c1261ad288749c32e02384228cc9ff23e84e
Luis Felipe <luisfelipe@MACBOOK-PRO-DE-ADESKTOP.local>
2022-09-14 17:00:53 -0300

    implementação de getters e setters

--- src/Conta.java
+++ src/Conta.java
@@ -36,4 +36,16 @@ public class Conta {
     public int getNumero() {
         return this.numero;
     }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 }
